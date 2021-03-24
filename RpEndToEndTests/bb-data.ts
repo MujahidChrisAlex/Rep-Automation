0611b8e3bf62cd5e8e1736d43e6f4079228a3a9e
MujahidChrisAlex <57789376+MujahidChrisAlex@users.noreply.github.com>
2021-03-24 14:05:09 +0500

    Add files via upload

---
+++ RpEndToEndTests/bb-data.ts
@@ -0,0 +1,32 @@
+exports.bbDataStaging = {
+  homePageUrl: "https://staging.burtsbees.com/",
+  productPageUrl:
+    "https://staging.burtsbees.com/product/sensitive-facial-cleanser/01430-00.html",
+  mySubscriptionsUrl: "https://staging.burtsbees.com/MySubscriptions#/",
+  mySubscriptionsListUrl: "https://staging.burtsbees.com/MySubscriptions#/list",
+  registerPageUrl: "https://staging.burtsbees.com/register",
+  email: "superizvi@gmail.com",
+  password: "Pass@word1",
+  productionEmail: "alex@test.com",
+  productionPassword: "Pass@word1",
+  firstName: "Mujahid",
+  lastName: "Rizvi",
+  registerEmail: "qa.automation.",
+  registerEmailB: "@replenium.com",
+};
+
+exports.bbDataProd = {
+  homePageUrl: "https://www.burtsbees.com/",
+  productPageUrl:
+    "https://www.burtsbees.com/product/hydrating-overnight-mask/792850904880.html",
+  mySubscriptionsUrl: "https://www.burtsbees.com/MySubscriptions#/",
+  mySubscriptionsListUrl: "https://www.burtsbees.com/MySubscriptions#/list",
+  email: "superizvi@gmail.com",
+  password: "Pass@word1",
+  productionEmail: "ohvictori@gmail.com",
+  productionPassword: "Test1234-",
+  firstName: "Mujahid",
+  lastName: "Rizvi",
+  registerEmail: "qa.automation.",
+  registerEmailB: "@replenium.com",
+};
